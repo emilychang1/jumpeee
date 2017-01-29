95da8abf057983c8955c9e89a85ef8d16a656f83
Alvin Wan <hi@alvinwan.com>
2017-01-28 23:10:57 -0800

    [server] removed cocos2d

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-var app = require('express')();
+var express = require('express');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+var app = express();
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
