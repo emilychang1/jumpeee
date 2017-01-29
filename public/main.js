535b1498d9996edd493561e09649cb6aa47f4026
Emily Chang <emilychang@airbears2-10-142-167-117.airbears2.1918.berkeley.edu>
2017-01-29 01:14:57 -0800

    circle bounces

--- public/main.js
+++ public/main.js
@@ -1,1 +1,58 @@
-alert('hello')
+
+window.onload = function() {
+
+	var canvas = document.getElementById("canvas"); 
+	canvas.width = document.body.clientWidth;
+	canvas.height = document.body.clientHeight;
+	var ctx = canvas.getContext("2d"); 
+	var isJumping = false;
+	var time = 0;
+	var timeFinish;
+	var timeStart;
+	var yStart;
+	var c = {  
+		x:5, 
+		y:125, 
+		r:25 
+	}
+	
+	var redraw = function(){ 
+		ctx.clearRect(0, 0, canvas.width, canvas.height); 
+		ctx.beginPath();  
+		ctx.arc(c.x, c.y, c.r, 0, Math.PI*2); 
+		ctx.closePath(); 
+		ctx.fill(); 
+		requestAnimationFrame(redraw);
+	}
+
+	function update(){
+		c.x = c.x + 1;
+		time = time + 10; 
+
+		if (isJumping == true) {
+			jump()
+		}
+	}
+	
+	window.onkeypress = function(e) {
+		if (isJumping == false) {
+			isJumping = true;
+			timeStart = time;
+			timeFinish = timeStart + 1000;
+			yStart = c.y;
+		}
+	}
+
+	function jump(){
+		if(time >= timeFinish) {
+			isJumping = false;
+		}
+		console.log(time, timeFinish);
+		var height = -((timeFinish - timeStart)/2 + timeStart - time)/100; 
+		c.y = c.y + height;
+	}
+
+	redraw(); 
+	setInterval(update, 10); 
+
+}
