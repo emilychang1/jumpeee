0821bbb45d61b53078f889969ef985a8a6fc402b
Alvin Wan <hi@alvinwan.com>
2017-01-28 23:14:01 -0800

    [server] added statics folder

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var app = express();
+app.use(express.static('public'))
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
