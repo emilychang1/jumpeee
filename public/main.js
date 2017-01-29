c79209157c0d0a2f304628d54b487f5c2026a85b
Alvin Wan <hi@alvinwan.com>
2017-01-29 01:49:09 -0800

    [client] added Player abstraction

--- public/main.js
+++ public/main.js
@@ -4,55 +4,34 @@ window.onload = function() {
 	var canvas = document.getElementById("canvas"); 
 	canvas.width = document.body.clientWidth;
 	canvas.height = document.body.clientHeight;
-	var ctx = canvas.getContext("2d"); 
-	var isJumping = false;
+	var ctx = canvas.getContext("2d");
+	var currentPlayer = new Player();
+	var players = [];
 	var time = 0;
-	var timeFinish;
-	var timeStart;
-	var yStart;
-	var c = {  
-		x:5, 
-		y:125, 
-		r:25 
-	}
+
+	players.push(currentPlayer);
 	
 	var redraw = function(){ 
-		ctx.clearRect(0, 0, canvas.width, canvas.height); 
-		ctx.beginPath();  
-		ctx.arc(c.x, c.y, c.r, 0, Math.PI*2); 
-		ctx.closePath(); 
-		ctx.fill(); 
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        for (var i=0; i<players.length; i++) {
+            var player = players[i];
+            player.draw(ctx);
+        }
 		requestAnimationFrame(redraw);
 	}
 
-	function update(){
-		c.x = c.x + 1;
-		time = time + 10; 
-
-		if (isJumping == true) {
-			jump()
-		}
+	function update() {
+        for (var i=0; i<players.length; i++) {
+            var player = players[i];
+            player.update(time);
+        }
+		time = time + 10;
 	}
 	
 	window.onkeypress = function(e) {
-		if (isJumping == false) {
-			isJumping = true;
-			timeStart = time;
-			timeFinish = timeStart + 1000;
-			yStart = c.y;
-		}
-	}
-
-	function jump(){
-		if(time >= timeFinish) {
-			isJumping = false;
-		}
-		console.log(time, timeFinish);
-		var height = -((timeFinish - timeStart)/2 + timeStart - time)/100; 
-		c.y = c.y + height;
+		currentPlayer.startJump(time);
 	}
 
 	redraw(); 
-	setInterval(update, 10); 
-
+	setInterval(update, 10);
 }
---
+++ public/player.js
@@ -0,0 +1,52 @@
+/**
+ * Player Management
+ * Creates a player, whether controlled by the current player or by another.
+ */
+
+
+JUMP_DURATION = 500;
+JUMP_SCALING = 2;
+
+
+function Player() {
+    var isJumping = false;
+	var timeFinish;
+	var timeStart;
+	var yStart;
+	var x = 50;
+	var y = 125;
+	var r = 25;
+
+	this.draw = function(ctx) {
+		ctx.beginPath();
+		ctx.arc(x, y, r, 0, Math.PI*2);
+		ctx.closePath();
+		ctx.fill();
+    }
+
+    this.update = function(time) {
+        if (isJumping == true) {
+			this.updateJump(time);
+		}
+    }
+
+    this.startJump = function(time) {
+        if (isJumping == false) {
+			isJumping = true;
+			timeStart = time;
+			timeFinish = timeStart + JUMP_DURATION;
+			yStart = y;
+		}
+    }
+
+    this.updateJump = function(time){
+		if(time >= timeFinish) {
+			isJumping = false;
+			y = yStart;
+			return;
+		}
+		var height = -((timeFinish - timeStart)/2 + timeStart - time)/100 * 2;
+		console.log(y, y + height);
+		y = y + height;
+	}
+}
